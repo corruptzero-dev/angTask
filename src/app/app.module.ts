7284e66f995d0fa6a0534e9221cd122b62d60405
corruptzero-dev <74492746+corruptzero-dev@users.noreply.github.com>
2022-02-09 11:26:03 +0300

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { PeopleComponent } from './people.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PeopleComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/people.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+      
+@Component({
+    selector: 'people-comp',
+    templateUrl: './app.component.html'
+})
+export class PeopleComponent { 
+    
+}
